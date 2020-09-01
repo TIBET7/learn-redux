d606f312056cac460138505b924d63495ae700f6
Dim <om1234777@gmail.com>
2020-09-01 17:02:32 +0300

    add redux bindActionCreators

--- src/index.js
+++ src/index.js
@@ -1,27 +1,23 @@
-import { createStore } from 'redux';
+import { createStore, bindActionCreators } from 'redux';
 import reducer from './reducer';
 import * as actions from './actions';
 
 const store = createStore(reducer);
 
-const { inc, dec, rnd } = actions;
 const { dispatch } = store;
 
-const bindActionCreator = (creator, dispatch) => (...args) => {
-  dispatch(creator(...args));
-}
+const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
 
-const incDispatch = bindActionCreator(inc, dispatch);
-const decDispatch = bindActionCreator(dec, dispatch);
-const rndDispatch = bindActionCreator(rnd, dispatch);
+// const decDispatch = bindActionCreators(dec, dispatch);
+// const rndDispatch = bindActionCreators(rnd, dispatch);
 
-document.getElementById('inc').addEventListener('click', incDispatch);
+document.getElementById('inc').addEventListener('click', inc);
 
-document.getElementById('dec').addEventListener('click', decDispatch);
+document.getElementById('dec').addEventListener('click', dec);
 
 document.getElementById('rnd').addEventListener('click', () => {
   const value = Math.floor(Math.random() * 10);
-  rndDispatch(value);
+  rnd(value);
 });
 
 const update = () => {
