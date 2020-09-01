c3c5f477f333cfbbb8ca4e586cac78a37b8def1d
Dim <om1234777@gmail.com>
2020-09-01 19:11:58 +0300

    modify counter

---
+++ src/components/counter.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { inc, dec, rnd } from '../actions';
+
+const Counter = ({counter, inc, dec, rnd}) => {
+    return (
+        <div className="jumbotron" id="root">
+            <h1>{counter}</h1>
+            <button onClick={dec} className="btn btn-primary">DEC</button>
+            <button onClick={inc} className="btn btn-primary">INC</button>
+            <button onClick={rnd} className="btn btn-primary">RND</button>
+        </div>
+    );
+}
+const mapStateToProps = (state) => {
+    return {
+        counter: state
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+      inc: () => dispatch(inc()), 
+      dec: () => dispatch(dec()), 
+      rnd: () => {
+          const value = Math.floor(Math.random() * 10);
+          dispatch(rnd(value))
+      } 
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
